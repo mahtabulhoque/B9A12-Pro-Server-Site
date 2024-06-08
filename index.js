b6ba54db151e46c9ba6c4a6ed88bedacd413afc9
hagghat <mahach0396@gmail.com>
2024-06-08 22:54:32 +0600

    4rt

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Mongodb
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i8q8q4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -26,33 +26,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const surveyCollection = client.db('Pro-Survey').collection('survey');
-    const userCollection = client.db('Pro-Survey').collection('users');
+    const surveyCollection = client.db("Pro-Survey").collection("survey");
+    const userCollection = client.db("Pro-Survey").collection("users");
 
-// User Related Api
-    app.post('/users', async (req, res) => {
+    // User Related Api
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query ={email:user.email}
+      console.log("Received user info:", user);
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
-      if (existingUser){
-        return res.send({message:'user already exist', insertedId:null})
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
       }
       const result = await userCollection.insertOne(user);
+      console.log("User inserted:", result);
       res.send(result);
-    })
+    });
 
-
-
-
-    app.get('/survey', async(req, res) => {
-        const result = await surveyCollection.find().toArray();
-        res.send(result);
-    })
-
+    app.get("/survey", async (req, res) => {
+      const result = await surveyCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -60,13 +60,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('B9A12 Assignment')
-})
+app.get("/", (req, res) => {
+  res.send("B9A12 Assignment");
+});
 
 app.listen(port, () => {
-    console.log(`B9A12 Assignment is running ${port}`);
-})
+  console.log(`B9A12 Assignment is running ${port}`);
+});
