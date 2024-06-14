1364aa9c866301897becfcf27ac5cda779eb6e32
hagghat <mahach0396@gmail.com>
2024-06-15 03:47:14 +0600

    9th

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173"
-      
-    ],
+    origin: ["http://localhost:5173"],
     credentials: true,
   })
 );
@@ -36,17 +35,18 @@ async function run() {
 
     // verify Admin middleware
 
-    const verifyAdmin = async (req,res,next)=>{
-      const user = req.user
-      const query = {email:user?.email}
-      const result = await userCollection.findOne(query)
-      if(!result || result?. role !== 'Admin')
-        return res.status(401).send({message:'forbidden access'})
-    }
+    const verifyAdmin = async (req, res, next) => {
+      const user = req.user;
+      const query = { email: user?.email };
+      const result = await userCollection.findOne(query);
+      if (!result || result?.role !== "Admin")
+        return res.status(401).send({ message: "forbidden access" });
+    };
 
     const surveyCollection = client.db("Pro-Survey").collection("survey");
     const userCollection = client.db("Pro-Survey").collection("users");
-    const votesCollection = client.db("Pro-Survey").collection("vote")
+    const votesCollection = client.db("Pro-Survey").collection("vote");
+    const paymentCollection = client.db('Pro-Survey').collection('payment')
 
     // User Related Api
     app.put("/user", async (req, res) => {
@@ -54,7 +54,7 @@ async function run() {
       try {
         const isExist = await userCollection.findOne({ email: user?.email });
         if (isExist) return res.send(isExist);
-    
+
         const options = { upsert: true };
         const query = { email: user?.email };
         const updateDoc = {
@@ -63,8 +63,12 @@ async function run() {
             timestamp: Date.now(),
           },
         };
-    
-        const result = await userCollection.updateOne(query, updateDoc, options);
+
+        const result = await userCollection.updateOne(
+          query,
+          updateDoc,
+          options
+        );
         res.send(result);
       } catch (error) {
         console.error("Error updating user:", error);
@@ -72,186 +76,250 @@ async function run() {
       }
     });
 
-  //  get a user by email
+    //  get a user by email
 
-  app.get('/user/:email', async (req,res)=>{
-    const email = req.params.email
-    const result = await userCollection.findOne({email})
-    res.send(result)
-  })
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send(result);
+    });
 
+    //  get all users from db
 
-  //  get all users from db
-
-  app.get('/users', async (req,res)=>{
-    const result = await userCollection.find().toArray()
-    res.send(result)
-  })
-
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
     // update user role
 
-    app.patch('/users/update/:email', async (req, res) => {
+    app.patch("/users/update/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
       const query = { email };
       const updateDoc = {
         $set: { ...user, timestamp: new Date() },
       };
-      
+
       try {
         const result = await userCollection.updateOne(query, updateDoc);
         if (result.modifiedCount > 0) {
-          res.send({ success: true, message: 'User role updated successfully' });
+          res.send({
+            success: true,
+            message: "User role updated successfully",
+          });
         } else {
-          res.status(404).send({ success: false, message: 'User not found' });
+          res.status(404).send({ success: false, message: "User not found" });
         }
       } catch (error) {
-        res.status(500).send({ success: false, message: 'Error updating user role', error });
+        res
+          .status(500)
+          .send({ success: false, message: "Error updating user role", error });
       }
     });
-    
 
+    // Survey creation API
 
+    app.post("/create", async (req, res) => {
+      const createdSurvey = req.body;
+      const result = await surveyCollection.insertOne(createdSurvey);
+      res.send(result);
+    });
 
-   // Survey creation API
+    // Get all survey
+    app.get("/survey", async (req, res) => {
+      const filterType = req.query.filterType;
 
-   app.post('/create', async(req,res)=>{
-    const createdSurvey = req.body
-    const result = await surveyCollection.insertOne(createdSurvey)
-    res.send(result)
-   })
- 
+      try {
+        let result;
+        if (filterType === "mostVoted") {
+          result = await surveyCollection
+            .find()
+            .sort({ voteCount: -1 })
+            .toArray();
+        } else if (filterType === "latest") {
+          result = await surveyCollection
+            .find()
+            .sort({ timestamp: -1 })
+            .toArray();
+        } else {
+          result = await surveyCollection.find().toArray();
+        }
 
- 
-  
-    
-
-
-
-
-    
-
-
-    
-
-// Get all survey
-app.get('/survey', async (req, res) => {
-  const filterType = req.query.filterType;
-
-  try {
-    let result;
-    if (filterType === 'mostVoted') {
-      // Find and sort surveys by voteCount in descending order 
-      result = await surveyCollection.find().sort({ voteCount: -1 }).toArray();
-    } else if (filterType === 'latest') {
-      // Find and sort surveys by creation timestamp in descending order
-      result = await surveyCollection.find().sort({ timestamp: -1 }).toArray();
-    } else {
-      // Fetch all surveys if no filterType or invalid filterType is provided
-      result = await surveyCollection.find().toArray();
-    }
-    
-    res.send(result);
-  } catch (error) {
-    console.error("Error fetching surveys:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
-
-
-  //  Get a survey by Id
-
-  app.get('/survey/:id', async (req, res) => {
-    const id = req.params.id;
-  
-    // Check if the ID is a valid 24-character hexadecimal string
-    if (!ObjectId.isValid(id)) {
-      return res.status(400).send({ error: 'Invalid survey ID format' });
-    }
-  
-    const query = { _id: new ObjectId(id) };
-    
-    try {
-      const result = await surveyCollection.findOne(query);
-      if (!result) {
-        return res.status(404).send({ error: 'Survey not found' });
+        res.send(result);
+      } catch (error) {
+        console.error("Error fetching surveys:", error);
+        res.status(500).json({ error: "Internal Server Error" });
       }
+    });
+
+
+    // filter survey
+
+    app.get('/surveys', async (req, res) => {
+      const filter = req.query.filter;
+      const sort = req.query.sort
+      let query = {};
+      if (filter) {
+        query = { ...query, category: filter }
+      }
+      let options = {};
+      if (sort) {
+        options = { sort: { voteCount: sort === "asc" ? 1 : -1 } }
+      }
+      const result = await surveyCollection.find(query, options).toArray()
+      res.send(result)
+    })
+
+    app.post('/api/submit-survey', async (req, res) => {
+      const surveyAnswers = req.body;
+      // Process and store the survey answers in the database if necessary
+      // For this example, we simply log them
+      console.log("Received survey answers:", surveyAnswers);
+      res.status(200).send("Survey submitted successfully");
+    });
+  
+
+    //  Get a survey by Id
+
+    app.get("/survey/:id", async (req, res) => {
+      const id = req.params.id;
+
+      if (!ObjectId.isValid(id)) {
+        return res.status(400).send({ error: "Invalid survey ID format" });
+      }
+
+      const query = { _id: new ObjectId(id) };
+
+      try {
+        const result = await surveyCollection.findOne(query);
+        if (!result) {
+          return res.status(404).send({ error: "Survey not found" });
+        }
+        res.send(result);
+      } catch (error) {
+        res
+          .status(500)
+          .send({ error: "An error occurred while retrieving the survey" });
+      }
+    });
+
+    // get survey by email
+
+    app.get("/surveys/:email", async (req, res) => {
+      try {
+        const email = req.params.email;
+
+        const query = { surveyor_email: email }; // Use the email parameter from the request
+        const viewSurvey = await surveyCollection.find(query).toArray();
+        res.send(viewSurvey);
+      } catch (error) {
+        res.status(500).send({
+          message: "An error occurred while fetching surveys",
+          error: error.message,
+        });
+      }
+    });
+
+    // update a survey
+
+    app.put("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      const result = await surveyCollection.updateOne(
+        {
+          _id: new ObjectId(id),
+        },
+        { $set: updateData },
+        { upsert: true }
+      );
       res.send(result);
-    } catch (error) {
-      res.status(500).send({ error: 'An error occurred while retrieving the survey' });
-    }
-  });
+    });
 
-  // get survey by email
+    // for vote survey
+    app.post("/votes", async (req, res) => {
+      try {
+        const voteSurvey = req.body;
+        const voteId = voteSurvey.voteId;
 
-  app.get('/surveys/:email', async (req, res) => {
-    try {
-      const email = req.params.email;
-      
-      const query = { surveyor_email: email }; // Use the email parameter from the request
-      const viewSurvey = await surveyCollection.find(query).toArray();
-      res.send(viewSurvey); 
-    } catch (error) {
-      res.status(500).send({ message: 'An error occurred while fetching surveys', error: error.message });
-    }
-  });
+        const result = await votesCollection.insertOne(voteSurvey);
 
+        const voteQuery = { _id: new ObjectId(voteId) };
+        const updateDoc = {
+          $inc: { voteCount: 1 },
+        };
+        const updateVoteCount = await surveyCollection.updateOne(
+          voteQuery,
+          updateDoc
+        );
 
-  // update a survey
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
 
-  app.put('/update/:id', async (req, res)=>{
-    const id = req.params.id;
-    const updateData= req.body;
-    const result = await surveyCollection.updateOne(
-      {
-        _id:new ObjectId(id)
-      },
-      {$set: updateData},
-      {upsert:true}
-    );
-    res.send(result);
-  })
-  
-  
-  
-  
-  
-
-
-   // for vote survey
-   app.post('/votes', async (req, res) => {
-    try {
-      const voteSurvey = req.body;
-      const voteId = voteSurvey.voteId;
-      
-      // Insert the voteSurvey data into votesCollection
-      const result = await votesCollection.insertOne(voteSurvey);
-  
-      // Update the voteCount in surveyCollection
-      const voteQuery = { _id: new ObjectId(voteId) };
-      const updateDoc = {
-        $inc: { voteCount: 1 }, // Increment voteCount by 1
-      };
-      const updateVoteCount = await surveyCollection.updateOne(voteQuery, updateDoc);
-  
-      // Send the result back to the client
-      res.send(result);
-    } catch (error) {
-      // Handle errors
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  
-
+    // payment related API
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100); // Convert to cents or smallest currency unit
     
+      try {
+        const paymentIntent = await stripe.paymentIntents.create({
+          amount: amount,
+          currency: 'usd',
+          payment_method_types: ['card'],
+        });
+    
+        res.send({
+          clientSecret: paymentIntent.client_secret,
+        });
+      } catch (error) {
+        console.error('Error creating payment intent:', error);
+        res.status(500).json({ error: 'Failed to create payment intent' });
+      }
+    });
+
+    app.get('/payments', async (req, res) => {
+      const result = await paymentCollection.find().toArray();
+      res.send(result)
+
+    })
+
+    app.get('/payments/:email', async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" })
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+
+      try {
+        const paymentResult = await paymentCollection.insertOne(payment);
+        console.log("Payment Info", payment);
+        const userUpdateResult = await userCollection.updateOne(
+          { email: payment?.email },
+          {
+            $set: { role: 'Pro-user' }
+          }
+        );
+
+        res.send({ paymentResult, userUpdateResult });
+      } catch (error) {
+        console.error('Error processing payment:', error);
+        res.status(500).send({ message: 'Internal server error' });
+      }
+    });
 
 
 
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
